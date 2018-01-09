4ac00b866ccaaa697e0a56eb33c457b50362e56c
Vitoria Heliane <vitoriaheliane@gmail.com>
2018-01-09 13:19:53 -0300

    Ajeitando o problema do mapa

--- app/src/main/java/br/edu/ufcg/fidu/views/fragments/DonorSignupFragment.java
+++ app/src/main/java/br/edu/ufcg/fidu/views/fragments/DonorSignupFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
